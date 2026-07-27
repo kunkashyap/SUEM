1eafa181bfa4a3b15f9b5f4613e63f0edca667d4
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-07-27 10:41:43 +0530

    testing backend

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,9 +1,32 @@
+// import axios from 'axios';
+
+// const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || '';
+// export const API = `${BACKEND_URL}/api`;
+
+// const api = axios.create({ baseURL: API });
+
+// api.interceptors.request.use((cfg) => {
+//   const t = localStorage.getItem('medsim_token');
+//   if (t) cfg.headers.Authorization = `Bearer ${t}`;
+//   return cfg;
+// });
+
+// export default api;
+
+
+
 import axios from 'axios';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || '';
+
+console.log("BACKEND_URL =", BACKEND_URL);
+console.log("API =", `${BACKEND_URL}/api`);
+
 export const API = `${BACKEND_URL}/api`;
 
-const api = axios.create({ baseURL: API });
+const api = axios.create({
+  baseURL: API,
+});
 
 api.interceptors.request.use((cfg) => {
   const t = localStorage.getItem('medsim_token');
