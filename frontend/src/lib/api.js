a5b07540e15841e71e982c2156495a57bfa7d25b
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-07-28 10:07:42 +0530

    fixes: backend to frontend api

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,24 +1,17 @@
-// import axios from 'axios';
-
-// const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || '';
-// export const API = `${BACKEND_URL}/api`;
-
-// const api = axios.create({ baseURL: API });
-
-// api.interceptors.request.use((cfg) => {
-//   const t = localStorage.getItem('medsim_token');
-//   if (t) cfg.headers.Authorization = `Bearer ${t}`;
-//   return cfg;
-// });
-
-// export default api;
-
-
-
 import axios from 'axios';
 
-const BACKEND_URL =
-  (process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000').replace(/\/$/, '');
+// Dynamically determine the backend URL based on env variables and environment fallbacks
+const getBackendUrl = () => {
+  if (process.env.REACT_APP_BACKEND_URL) {
+    return process.env.REACT_APP_BACKEND_URL;
+  }
+  if (process.env.NODE_ENV === 'production') {
+    return 'https://suem-backend.onrender.com';
+  }
+  return 'http://localhost:5000';
+};
+
+const BACKEND_URL = getBackendUrl().replace(/\/$/, '');
 
 console.log("[MedSim API] BACKEND_URL =", BACKEND_URL);
 console.log("[MedSim API] API BaseURL =", `${BACKEND_URL}/api`);
